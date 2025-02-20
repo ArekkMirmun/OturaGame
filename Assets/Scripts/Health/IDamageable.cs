0c282783b8d228100ff44ee0601ca30f33e98ac0
A5mol <166757038+A5mol@users.noreply.github.com>
2025-02-20 19:45:39 +0100

    Fix: Finalize PlayerHealth and PlayerHealthUI implementation

--- Assets/Scripts/Health/IDamageable.cs
+++ Assets/Scripts/Health/IDamageable.cs
@@ -1,17 +1,17 @@
-/// <summary>
-/// Interface to damage entities. It can be implemented for damage the player, enemies, objects etc.
-/// </summary>
-
-public interface IDamageable
+namespace Health
 {
     /// <summary>
-    /// To recieve damage
+    /// Interface to damage entities. It can be implemented for damage the player, enemies, objects etc.
     /// </summary>
-    /// <param name="amount"> amount of damage taken</param>
-    void TakeDamage(float amount);  
+
+    public interface IDamageable
+    {
+        /// <summary>
+        /// To recieve damage
+        /// </summary>
+        /// <param name="amount"> Amount of damage taken</param>
+        void TakeDamage(float amount);  
     
-    /// <summary>
-    /// For dying.
-    /// </summary>
-    void Die();  
+        void Die();  
+    }
 }
--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -1,111 +1,108 @@
 using System;
 using UnityEngine;
 
-
-/// <summary>
-/// Class for managing the player health
-/// Implements <see cref="IDamageable"/>
-/// </summary>
-public class PlayerHealth : MonoBehaviour, IDamageable
+namespace Health
 {
-    [SerializeField]private float maxHealth = 100f;
-    private float currentHealth;
-    
-    public float pasiveHealAmount = 1f;
-    public float tickSeconds = 5f;
-    private float lastTick;
-    
     /// <summary>
-    /// Event that is triggered whenever the player's health changes.
-    /// This event should be invoked whenever there is a change in the player's current health,
-    /// allowing other systems (e.g., UI or gameplay effects) to react to health updates.
+    /// Class for managing the player health.
+    /// Implements <see cref="IDamageable"/>
     /// </summary>
-    /// <remarks>
-    /// The event passes the new health value as a float when it is invoked.
-    /// </remarks>
-    public static event Action<float> OnChangePlayerHealth;
-
-    /// <summary>
-    /// Event that is triggered whenever the player's maximum health changes.
-    /// This event should be invoked when the player's maximum health value is updated.
-    /// </summary>
-    /// <remarks>
-    /// The event passes the new maximum health value as a float when it is invoked.
-    /// </remarks>
-    public static event Action<float> OnChangePlayerMaxHealth;
-    
-
-    void Start()
+    public class PlayerHealth : MonoBehaviour, IDamageable
     {
-        OnChangePlayerMaxHealth?.Invoke(maxHealth);
-        currentHealth = maxHealth/2;
-        OnChangePlayerHealth?.Invoke(currentHealth);
-        lastTick = Time.time;
-    }
+        [SerializeField]private float maxHealth = 100f;
+        private float currentHealth;
+    
+        public float pasiveHealAmount = 1f;
+        public float tickSeconds = 5f;
+        private float lastTick;
+    
+        /// <summary>
+        /// Event that is triggered whenever the player's health changes.
+        /// This event should be invoked whenever there is a change in the player's current health,
+        /// allowing other systems (e.g., UI or gameplay effects) to react to health updates.
+        /// </summary>
+        /// <remarks>
+        /// The event passes the new health value as a float when it is invoked.
+        /// </remarks>
+        public static event Action<float> OnChangePlayerHealth;
 
-    /// <summary>
-    /// Take damage, make it so it's not less than 0.
-    /// Invoke an event <see cref="OnChangePlayerHealth"/>
-    /// If currentHealth is 0 call Die <see cref="Die"/>
-    /// </summary>
-    /// <param name="amount">amount of damage taken</param>
-    public void TakeDamage(float amount)
-    {
-        currentHealth -= amount;
-        currentHealth = currentHealth > 0 ? currentHealth : 0;
-        OnChangePlayerHealth?.Invoke(currentHealth);
-        if (currentHealth <= 0)
+        /// <summary>
+        /// Event that is triggered whenever the player's maximum health changes.
+        /// This event should be invoked when the player's maximum health value is updated.
+        /// </summary>
+        /// <remarks>
+        /// The event passes the new maximum health value as a float when it is invoked.
+        /// </remarks>
+        public static event Action<float> OnChangePlayerMaxHealth;
+    
+
+        void Start()
         {
-            Debug.Log(name + " vida: " + currentHealth);
-            Die(); 
-        }
-    }
-
-    /// <summary>
-    /// Is called when you make it die
-    /// </summary>
-    public void Die()
-    {
-        Debug.Log("El jugador ha muerto.");
-        // TODO Death logic and/or another event to delegate the efects of the death.
-    }
-
-    /// <summary>
-    /// Heal a amount and make it's not more than maxHealth.
-    /// Send an event <see cref="OnChangePlayerHealth"/>
-    /// </summary>
-    /// <param name="amount">Amount of heal</param>
-    public void Heal(float amount)
-    {
-        currentHealth += amount;
-        currentHealth = currentHealth > maxHealth ? maxHealth : currentHealth;
-        Debug.Log("Vida: "+currentHealth);
-        OnChangePlayerHealth?.Invoke(currentHealth);
-    }
-
-    public void AddMaxHealth(float amount)
-    {
-        maxHealth += amount;
-        maxHealth = maxHealth > 1? maxHealth : 1;
-        OnChangePlayerMaxHealth?.Invoke(maxHealth);
-    }
-
-    
-    /// <summary>
-    /// This method is called every frame to update the state of the player.
-    /// It checks if enough time has passed since the last heal tick, 
-    /// and if so, it applies passive healing.
-    /// </summary>
-    void Update()
-    {
-        // Check if enough time has passed since the last healing tick
-        if (Time.time - lastTick > tickSeconds)
-        {
-            // Update the last tick time
+            OnChangePlayerMaxHealth?.Invoke(maxHealth);
+            currentHealth = maxHealth;
+            //currentHealth = maxHealth/2;
+            OnChangePlayerHealth?.Invoke(currentHealth);
+        
             lastTick = Time.time;
+        }
 
-            // Apply passive healing to the player
-            Heal(pasiveHealAmount);
+        /// <summary>
+        /// Take damage, make it so it's not less than 0.
+        /// Invoke an event <see cref="OnChangePlayerHealth"/>
+        /// If currentHealth is 0 call Die <see cref="Die"/>
+        /// </summary>
+        /// <param name="amount">amount of damage taken</param>
+        public void TakeDamage(float amount)
+        {
+            currentHealth -= amount;
+            currentHealth = currentHealth > 0 ? currentHealth : 0;
+            OnChangePlayerHealth?.Invoke(currentHealth);
+            if (currentHealth <= 0)
+            {
+                Debug.Log(name + " vida: " + currentHealth);
+                Die(); 
+            }
+        }
+    
+        public void Die()
+        {
+            Debug.Log("El jugador ha muerto.");
+            // TODO Death logic and/or another event to delegate the efects of the death.
+        }
+
+        /// <summary>
+        /// Heal a amount and make it's not more than maxHealth.
+        /// Invoke event <see cref="OnChangePlayerHealth"/>
+        /// </summary>
+        /// <param name="amount">Amount of heal</param>
+        public void Heal(float amount)
+        {
+            currentHealth += amount;
+            currentHealth = currentHealth > maxHealth ? maxHealth : currentHealth;
+            Debug.Log("Vida: "+currentHealth);
+            OnChangePlayerHealth?.Invoke(currentHealth);
+        }
+
+        /// <summary>
+        /// Increase MaxHealth in a amount.
+        /// Doesn't let in go less than 1.
+        /// Invoke event <see cref="OnChangePlayerMaxHealth"/>
+        /// </summary>
+        /// <param name="amount"></param>
+        public void AddMaxHealth(float amount)
+        {
+            maxHealth += amount;
+            maxHealth = maxHealth > 1? maxHealth : 1;
+            OnChangePlayerMaxHealth?.Invoke(maxHealth);
+        }
+    
+        void Update()
+        {
+            if (Time.time - lastTick > tickSeconds)
+            {
+                lastTick = Time.time;
+                Heal(pasiveHealAmount);
+            }
         }
     }
 }
